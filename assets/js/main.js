0b163ad06d459aef8edf1fbef6036f4eef7ebbac
KotchakornM <mill1704.bn@gmail.com>
2019-10-20 04:28:26 +0700

    DynamicPageResume

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,71 @@
-function showTheme(){
-    var x = document.getElementById("bright");
-    var y = document.getElementById("dark");
-    document.body.style.backgroundColor = "#000";
-    document.body.style.backgroundImage = "none";
-    document.body.style.color = "#fff";
-    if(x.style.display == "block"){
-        x.style.display = "none";
-    }
-    else {
-        x.style.display = "block";
-        y.style.display = "none";  
-    }
-}
-function closeTheme(){
-    var x = document.getElementById("bright");
-    var y = document.getElementById("dark");
-    document.body.style.backgroundImage = "url('/assets/img/bg-img.jpg')";
-    document.body.style.color = "#000";
-    if(y.style.display == "block"){
-        y.style.display = "none";  
-    }
-    else {
-        y.style.display = "block";
-        x.style.display = "none";
-    }
-}
-function onHover(){
-    var date = new Date();
-    document.getElementById("date").innerHTML = date;
-    document.getElementById("hover").style.color = "#67a272";
-}
-function onOut(){
-    document.getElementById("date").innerHTML = "";
-    document.getElementById("hover").style.color = "#e84646";
-}
+// theme dark
+$(document).ready(function() {
+    $('#dark').click(function(){
+        $('#bright').show();
+        $('#dark').hide();
+        $('#body').css("backgroundColor", "#000");
+        $('#body').css("backgroundImage", "none");
+        $('#body').css("color", "#fff");
+    });
+});
+// theme bright
+$(document).ready(function() {
+    $('#bright').click(function(){
+        $('#dark').show();
+        $('#bright').hide();
+        $('#body').css("backgroundImage", "url('/assets/img/bg-img.jpg')");
+        $('#body').css("color", "#000");
+    });
+});
+//mouse event
+$(document).ready(function () {  
+    $('#mouseover').mouseover(function () { 
+        var date = new Date();
+        $('#hover').css("color", "#67a272");
+        $('#date').text(date);
+    });
 
+    $('#mouseover').mouseout(function () { 
+        $('#hover').css("color", "#e84646");
+        $('#date').text("");
+    });
+});
+//change name
+$(document).ready(function() {
+  $("#changeName").click(function() {
+    var fname = $("#fname").val();
+    $("#name").text(fname);
+    $("#fname").val("");
+    console.log(fname);
+  });
+});
+// add hard skill
+$(document).ready(function() {
+  $("#btnAdd1").click(function(e) {
+    var list = $("#hSkill").val();
+    $("<li>")
+      .text(list)
+      .prependTo("#hardSkill");
+    $("#hSkill").val("");
+  });
+});
+// add soft skill
+$(document).ready(function() {
+  $("#btnAdd2").click(function(e) {
+    var list = $("#sSkill").val();
+    $("<li>")
+      .text(list)
+      .prependTo("#softSkill");
+    $("#sSkill").val("");
+  });
+});
+//delete skill
+$(document).ready(function() {
+  $("#softSkill, #hardSkill").on("click", "li", function() {
+    $(this).toggleClass("select");
+  });
+
+  $("#btnRemove").click(function() {
+    $("li.select").remove();
+  });
+});
